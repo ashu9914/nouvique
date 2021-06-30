c31a2ff667608d4fa214c99f3cac13c7501beb95
Jim Carty <cartyjim1@gmail.com>
2021-06-30 22:46:06 +0100

    improvements to setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 cd backend
-mkvirtualenv venv --python=/usr/bin/python3.6
+mkvirtualenv venv --python=$(which python3.7)
 workon venv
 pip install -r requirements.txt
 python manage.py makemigrations backend
@@ -8,4 +8,5 @@ python manage.py migrate
 cd ..
 
 cd frontend/
+npm install
 npm run build
