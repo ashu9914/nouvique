e7e93b707eb045728c18d1c24deafa14f7d56386
Jim Carty <cartyjim1@gmail.com>
2021-07-27 18:35:06 +0100

    Update deploy_pythonanywhere.sh

--- deploy_pythonanywhere.sh
+++ deploy_pythonanywhere.sh
@@ -6,7 +6,7 @@ rm release.zip
 rm -r release
 
 # download newest
-wget https://github.com/QuestioWo/teleview/releases/download/latest/release.zip
+wget https://github.com/QuestioWo/nouvique/releases/download/latest/release.zip
 
 # setup release
 unzip release.zip
@@ -16,6 +16,6 @@ cp db.sqlite3 release/backend/
 
 # setup backend + dependencies
 cd release
-# cp backend/backend/wsgi.py /var/www/
-# mv /var/www/wsgi.py /var/www/teleview_pythonanywhere_com_wsgi.py
+cp backend/backend/wsgi.py /var/www/
+mv /var/www/wsgi.py /var/www/nouvique_pythonanywhere_com_wsgi.py
 . run_backend.sh n
