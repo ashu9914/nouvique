a454e0e43a074c68de09b17bb7c279bc1cf9c2bd
Jim Carty <cartyjim1@gmail.com>
2021-08-10 23:31:00 +0100

    Updating package version. Adding Support page and footer at bottom of page to access it

--- frontend/src/components/elements/BasePage.tsx
+++ frontend/src/components/elements/BasePage.tsx
@@ -6,6 +6,10 @@ import NavBar from './NavBar';
 import AlertBar from './AlertBar';
 
 import './BasePage.css';
+import { Col, Container, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+import PACKAGE_JSON from '../../../package.json';
 
 interface Props extends PageProps { }
 interface State { }
@@ -28,6 +32,23 @@ export default class BasePage extends React.PureComponent<Props, State> {
 
 					{this.props.children}
 				</div>
+
+				<footer className='footer mt-auto py-3 bg-dark text-white'>
+					<Container>
+						<Row>
+							<Col>
+								Nouvique 2021 v{PACKAGE_JSON.version}
+							</Col>
+							<Col>
+								<div style={{ float: 'right' }}>
+									<Link to='/support'>
+										Support
+								</Link>
+								</div>
+							</Col>
+						</Row>
+					</Container>
+				</footer>
 			</React.Fragment>
 		)
 	}
