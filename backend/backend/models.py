a3cf79f2970fc13af6998d446adc1a79d7b90ce6
Jim Carty <cartyjim1@gmail.com>
2021-08-11 15:09:55 +0100

    Revert "featured fields"
    This reverts commit 890d4372949c8c77f61de7e70d588eccab3bd49f.

--- backend/backend/models.py
+++ backend/backend/models.py
@@ -6,8 +6,6 @@ from datetime import datetime, date
 class User(AbstractUser):
 	username = models.CharField(max_length=128, unique=True, primary_key=True, db_index=True)
 	email = models.EmailField(_('email address'))
-	is_seller = models.BooleanField(default=False)
-	is_featured = models.BooleanField(default=False)
 	location_town = models.CharField(max_length=128, default="")
 	location_country = models.CharField(max_length=64, default="")
 	location_postcode = models.CharField(max_length=8, default="")
@@ -27,7 +25,6 @@ class Item(models.Model):
 	seller = models.ForeignKey(User, on_delete=models.CASCADE)
 	bio = models.CharField(max_length=256)
 	upload_date = models.DateTimeField(auto_now_add=True)
-	is_featured = models.BooleanField(default=False)
 	# pictures : Set of strings/static stubs
 	shape = models.CharField(max_length=32)
 	colour = models.CharField(max_length=32)
